97efd75b1f0b0025567cc2e34d2e50d069521be8
Stephen_Lai <stephenlai2021@gmail.com>
2023-01-30 15:34:55 +0800

    commit

--- src/lib/supabase/config.js
+++ src/lib/supabase/config.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/auth-helpers-sveltekit"
 import {
-	PUBLIC_SUPABASE_ANON_KEY,
 	PUBLIC_SUPABASE_URL,
+	PUBLIC_SUPABASE_ANON_KEY,
 } from "$env/static/public"
 
 export const supabaseClient = createClient(
