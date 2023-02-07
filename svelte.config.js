fd8f48bebc2150c207f8328737978cdfb9c331d4
Stephen_Lai <stephenlai2021@gmail.com>
2023-02-07 13:42:00 +0800

    commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
